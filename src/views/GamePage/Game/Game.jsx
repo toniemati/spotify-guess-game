7332b3af8e87ad8402bc3177b5c8b5f73df0444c
toniemati <matikas2000@gmail.com>
2021-09-21 22:14:35 +0200

    playing song for only difficulty time, checking answer and displaying score

--- src/views/GamePage/Game/Game.jsx
+++ src/views/GamePage/Game/Game.jsx
@@ -1,63 +1,137 @@
 import './Game.css';
 import { useStateValue } from '../../../StateProvider';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import LoopIcon from '@material-ui/icons/Loop';
 
 const Game = ({ playlist, difficulty }) => {
   const [{ spotify }] = useStateValue();
   const [tracks, setTracks] = useState([]);
-  const [gameStarted, setGameStarted] = useState(false);
+  
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [isTimerStarted, setIsTimerStared] = useState(false);
+  const [isGuessing, setIsGuessing] = useState(false);
 
-  const [fourTracks, setFourTracks] = useState([]);
-  const [currentTrack, setCurrentTrack] = useState(null);
   const [usedTracks, setUsedTracks] = useState([]);
+  const [currentFourTracks, setCurrentFourTracks] = useState([]);
+  const [currentTrack, setCurrentTrack] = useState(null);
+  
+  const [maxRound, setMaxRound] = useState(10);
+  const [round, setRounds] = useState(0);
+  const [points, setPoints] = useState(0);
+  const audio = useRef(null);
 
-  const [audio, setAudio] = useState(new Audio());
-  const [interval, setInterval] = useState(null);
-
-  const handleStartGame = () => {
-    console.log('start game');
-    setGameStarted(true);
-
-    setNewFourTracks();
-    playTrack();
-  }
-
-  const playTrack = () => {
-    const idx = Math.floor(Math.random() * fourTracks.length);
-    const track = fourTracks[idx];
-
-    if (usedTracks.includes(track)) {
-      setNewFourTracks();
-      playTrack();
+  const gameLoop = () => {
+    if (round >= maxRound) {
+      stopAudio();
       return;
     }
 
-    setUsedTracks([...usedTracks, track]);
-    setAudio(audio.src = track.track.preview_url);
-    audio.volume = 0.2;
-    audio.play();
+    setRounds(round + 1);
+
+    const fourTracks = getFourTracks();
+    const track = getOneTrack(fourTracks);
+
+    if (usedTracks.includes(track)) {
+      console.log('include');
+      gameLoop();
+      return;
+    }
+
+    console.log('not include');
+    
+    setCurrentFourTracks(fourTracks);
+    setCurrentTrack(track);
+    setUsedTracks(usedTracks => [...usedTracks, track]);
+
+    audio.current.src = track.track.preview_url;
+    audio.current.volume = 0.2;
+    setIsTimerStared(true);
+    audio.current.play();
+    
   }
 
-  const setNewFourTracks = () => {
-    const tempFourTracks = [];
-    
+  const handleGuess = (trackId) => {
+    setIsGuessing(true);
+    setIsTimerStared(false);
+    if (trackId === currentTrack.track.id) {
+      setPoints(points + 1);
+    }
+
+    gameLoop();
+  }
+
+  const getOneTrack = (tracks) => {
+    const idx = Math.floor(Math.random() * tracks.length);
+    const track = tracks[idx];
+
+    return track;
+  }
+
+  const getFourTracks = () => {
+    const fourTracks = [];
+
     while (fourTracks.length < 4) {
       const idx = Math.floor(Math.random() * tracks.length);
       const track = tracks[idx];
+
       if (!fourTracks.includes(track))
         fourTracks.push(track);
     }
 
-    setFourTracks(tempFourTracks);
+    return fourTracks;
   }
 
+  const stopAudio = () => {
+    if (
+      audio.current.src &&
+      !audio.current.paused
+    )
+      audio.current.pause();
+  }
+
+  //* Timer interval
+  useEffect(() => {
+    if (!isTimerStarted) return;
+
+    let timer = 0;
+
+    const interval = setInterval(() => {
+      if (isGuessing) {
+        clearInterval(interval);
+        setIsGuessing(false);
+        return;
+      } else if (timer > difficulty.time) {
+        clearInterval(interval);
+        handleGuess();
+        return;
+      }
+
+      console.log(timer);
+      timer++;
+    }, 1000);
+
+    return () => clearInterval(interval);
+
+  }, [isTimerStarted, isGuessing]);
+
+  //* Check Game State
+  useEffect(() => {
+    if (!isGameStarted) return;
+
+    if (tracks.length < 10)
+      setMaxRound(tracks.length);
+
+    gameLoop();
+  }, [isGameStarted]);
+
+  //* Getting tracks from api
   useEffect(() => {
     const getTracks = async () => {
       let tempTracks = [];
 
       for (let i = 0; i < playlist.tracks.total; i += 100) {
         const { items } = await spotify.getPlaylistTracks(playlist.id, { limit: 100, offset: i });
-        tempTracks = [...tempTracks, ...items];
+        tempTracks = [...tempTracks, ...items.filter((item) => item.track.preview_url)];
       }
       setTracks(tempTracks);
     }
@@ -72,13 +146,31 @@ const Game = ({ playlist, difficulty }) => {
         <h2>{difficulty.name}</h2>
       </div>
 
-      {!gameStarted &&
-        <button onClick={handleStartGame} className="game__startButton">Start</button>
+      {!tracks.length && (
+        <LoopIcon className="game__loopIcon" />
+      )}
+
+      {(!isGameStarted && tracks.length) ?
+        <button onClick={() => setIsGameStarted(true)} className="game__startButton">Start {tracks.length}</button> :
+        ''
       }
 
-      {gameStarted &&
-        <h1>Playing</h1>
+      {isGameStarted &&
+        <div>
+          <p>round: {round}</p>
+          <p>points: {points}</p>
+
+          {isTimerStarted && (
+            <div>
+              {currentFourTracks.map((track) => (
+                <p onClick={() => handleGuess(track.track.id)} key={track.track.id}>{track.track.name}</p>
+              ))}
+            </div>
+          )}
+        </div>
       }
+
+      <audio ref={audio} />
     </div>
   )
 }
